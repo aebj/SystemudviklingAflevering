a1bb4871d8e4db379610b48b8239d1f706707792
Seje27878 <Seje27878@edu.ucl.dk>
2021-10-24 14:00:32 +0200

    Price changes

--- console.php
+++ console.php
@@ -16,22 +16,22 @@ include('categories.php');
         <div class="column">
           <img src="img/ps5.png" alt="">
           <h2>Playstation 5 (digital)</h2>
-          <p>Pris 3299 kr</p>
+          <p>Pris: 3299 kr</p>
         </div>
         <div class="column">
           <img src="img/Xbox.png" alt="">
           <h2>Xbox Series X 1TB</h2>
-          <p>Pris 3799 kr</p>
+          <p>Pris: 3799 kr</p>
         </div>
         <div class="column">
           <img src="img/ps5.png" alt="">
           <h2>Playstation 5 (Standard)</h2>
-          <p>Pris 4099 kr</p>
+          <p>Pris: 4099 kr</p>
         </div>
         <div class="column">
           <img src="img/Xbox one s.png" alt="">
           <h2>Xbox One S 1TB</h2>
-          <p>Pris 2299 kr</p>
+          <p>Pris: 2299 kr</p>
         </div>
       </div>
     </div>
@@ -40,22 +40,22 @@ include('categories.php');
         <div class="column">
           <img src="img/Nintendo.png" alt="">
           <h2>Nintendo Switch</h2>
-          <p>Pris 3099 kr</p>
+          <p>Pris: 3099 kr</p>
         </div>
         <div class="column">
           <img src="img/ps4.png" alt="">
           <h2>Playstation 4 500 GB</h2>
-          <p>Pris 2299 kr</p>
+          <p>Pris: 2299 kr</p>
         </div>
         <div class="column">
           <img src="img/Retro.png" alt="">
           <h2>Go Retro! Portable spillekonsol</h2>
-          <p>Pris 299 kr</p>
+          <p>Pris: 299 kr</p>
         </div>
         <div class="column">
           <img src="img/Evercade.png" alt="">
           <h2>Evercade Premium Pack spillekonsol</h2>
-          <p>Pris 739 kr</p>
+          <p>Pris: 739 kr</p>
         </div>
       </div>
     </div>
