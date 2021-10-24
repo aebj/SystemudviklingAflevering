bf289febb226ffb0323a58f38c698eee208b209d
Seje27878 <Seje27878@edu.ucl.dk>
2021-10-24 13:54:54 +0200

    Updated tv & consol

--- console.php
+++ console.php
@@ -14,48 +14,48 @@ include('categories.php');
     <div class="pad">
       <div class="row">
         <div class="column">
-          <img src="img/acer1.png" alt="">
-          <h2>Acer Laptop</h2>
-          <p>Det her er en acer laptop</p>
+          <img src="img/ps5.png" alt="">
+          <h2>Playstation 5 (digital)</h2>
+          <p>Pris 3299 kr</p>
         </div>
         <div class="column">
-          <img src="img/acer1.png" alt="">
-          <h2>Acer Laptop</h2>
-          <p>Det her er en acer laptop</p>
+          <img src="img/Xbox.png" alt="">
+          <h2>Xbox Series X 1TB</h2>
+          <p>Pris 3799 kr</p>
         </div>
         <div class="column">
-          <img src="img/acer1.png" alt="">
-          <h2>Acer Laptop</h2>
-          <p>Det her er en acer laptop</p>
+          <img src="img/ps5.png" alt="">
+          <h2>Playstation 5 (Standard)</h2>
+          <p>Pris 4099 kr</p>
         </div>
         <div class="column">
-          <img src="img/acer1.png" alt="">
-          <h2>Acer Laptop</h2>
-          <p>Det her er en acer laptop</p>
+          <img src="img/Xbox one s.png" alt="">
+          <h2>Xbox One S 1TB</h2>
+          <p>Pris 2299 kr</p>
         </div>
       </div>
     </div>
     <div class="pad">
       <div class="row">
         <div class="column">
-          <img src="img/acer1.png" alt="">
-          <h2>Acer Laptop</h2>
-          <p>Det her er en acer laptop</p>
+          <img src="img/Nintendo.png" alt="">
+          <h2>Nintendo Switch</h2>
+          <p>Pris 3099 kr</p>
         </div>
         <div class="column">
-          <img src="img/acer1.png" alt="">
-          <h2>Acer Laptop</h2>
-          <p>Det her er en acer laptop</p>
+          <img src="img/ps4.png" alt="">
+          <h2>Playstation 4 500 GB</h2>
+          <p>Pris 2299 kr</p>
         </div>
         <div class="column">
-          <img src="img/acer1.png" alt="">
-          <h2>Acer Laptop</h2>
-          <p>Det her er en acer laptop</p>
+          <img src="img/Retro.png" alt="">
+          <h2>Go Retro! Portable spillekonsol</h2>
+          <p>Pris 299 kr</p>
         </div>
         <div class="column">
-          <img src="img/acer1.png" alt="">
-          <h2>Acer Laptop</h2>
-          <p>Det her er en acer laptop</p>
+          <img src="img/Evercade.png" alt="">
+          <h2>Evercade Premium Pack spillekonsol</h2>
+          <p>Pris 739 kr</p>
         </div>
       </div>
     </div>
--- tv.php
+++ tv.php
@@ -14,48 +14,48 @@ include('categories.php');
     <div class="pad">
       <div class="row">
         <div class="column">
-          <img src="img/acer1.png" alt="">
-          <h2>Acer Laptop</h2>
-          <p>Det her er en acer laptop</p>
+          <img src="img/Philips 32.png" alt="">
+          <h2>Philips 32"</h2>
+          <p>Pris 2899 kr</p>
         </div>
         <div class="column">
-          <img src="img/acer1.png" alt="">
-          <h2>Acer Laptop</h2>
-          <p>Det her er en acer laptop</p>
+          <img src="img/Samsung 65.png" alt="">
+          <h2>Samsung 65"</h2>
+          <p>Pris 16000 kr</p>
         </div>
         <div class="column">
-          <img src="img/acer1.png" alt="">
-          <h2>Acer Laptop</h2>
-          <p>Det her er en acer laptop</p>
+          <img src="img/TCL 65.png" alt="">
+          <h2>TCL 65"</h2>
+          <p>Pris 5999 kr</p>
         </div>
         <div class="column">
-          <img src="img/acer1.png" alt="">
-          <h2>Acer Laptop</h2>
-          <p>Det her er en acer laptop</p>
+          <img src="img/Sony 55.png" alt="">
+          <h2>Sony 55"</h2>
+          <p>Pris 11999 kr</p>
         </div>
       </div>
     </div>
     <div class="pad">
       <div class="row">
         <div class="column">
-          <img src="img/acer1.png" alt="">
-          <h2>Acer Laptop</h2>
-          <p>Det her er en acer laptop</p>
+          <img src="img/LG 55.png" alt="">
+          <h2>LG 55"</h2>
+          <p>Pris 7999 kr</p>
         </div>
         <div class="column">
-          <img src="img/acer1.png" alt="">
-          <h2>Acer Laptop</h2>
-          <p>Det her er en acer laptop</p>
+          <img src="img/Sony 65.png" alt="">
+          <h2>Sony 65"</h2>
+          <p>Pris 12999 kr</p>
         </div>
         <div class="column">
-          <img src="img/acer1.png" alt="">
-          <h2>Acer Laptop</h2>
-          <p>Det her er en acer laptop</p>
+          <img src="img/Philips 32.png" alt="">
+          <h2>Philips 42"</h2>
+          <p>Pris 2999 kr</p>
         </div>
         <div class="column">
-          <img src="img/acer1.png" alt="">
-          <h2>Acer Laptop</h2>
-          <p>Det her er en acer laptop</p>
+          <img src="img/Samsung 50.png" alt="">
+          <h2>Samsung 50"</h2>
+          <p>Pris 4999 kr</p>
         </div>
       </div>
     </div>
