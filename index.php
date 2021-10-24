3bcdb1746dca8e08371c4f63b84cb2977647bee3
aebj <aebj27313@edu.ucl.dk>
2021-10-24 12:46:56 +0200

    Product setup

--- index.php
+++ index.php
@@ -8,10 +8,57 @@ include('navbar.php');
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="/css/master.css">
+    <link rel="stylesheet" href="css/index.css">
     <title></title>
   </head>
   <body>
-    
+    <div class="pad">
+      <div class="row">
+        <div class="column">
+          <img src="img/acer1.png" alt="">
+          <h2>Acer Laptop</h2>
+          <p>Det her er en acer laptop</p>
+        </div>
+        <div class="column">
+          <img src="img/acer1.png" alt="">
+          <h2>Acer Laptop</h2>
+          <p>Det her er en acer laptop</p>
+        </div>
+        <div class="column">
+          <img src="img/acer1.png" alt="">
+          <h2>Acer Laptop</h2>
+          <p>Det her er en acer laptop</p>
+        </div>
+        <div class="column">
+          <img src="img/acer1.png" alt="">
+          <h2>Acer Laptop</h2>
+          <p>Det her er en acer laptop</p>
+        </div>
+      </div>
+    </div>
+    <div class="pad">
+      <div class="row">
+        <div class="column">
+          <img src="img/acer1.png" alt="">
+          <h2>Acer Laptop</h2>
+          <p>Det her er en acer laptop</p>
+        </div>
+        <div class="column">
+          <img src="img/acer1.png" alt="">
+          <h2>Acer Laptop</h2>
+          <p>Det her er en acer laptop</p>
+        </div>
+        <div class="column">
+          <img src="img/acer1.png" alt="">
+          <h2>Acer Laptop</h2>
+          <p>Det her er en acer laptop</p>
+        </div>
+        <div class="column">
+          <img src="img/acer1.png" alt="">
+          <h2>Acer Laptop</h2>
+          <p>Det her er en acer laptop</p>
+        </div>
+      </div>
+    </div>
   </body>
 </html>
