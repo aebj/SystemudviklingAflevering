0eee1a2e71c96dd5bbb2c56fb216f29ffbc7c011
aebj <aebj27313@edu.ucl.dk>
2021-10-24 11:09:54 +0200

    Initial pages

---
+++ navbar.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="">
+    <title></title>
+  </head>
+  <body>
+    <div class="">
+      <a href="#">Forside</a>
+      <a href="#">Kategorier</a>
+      <a href="#">Om os</a>
+      <a href="#">Kurv</a>
+    </div>
+  </body>
+</html>
