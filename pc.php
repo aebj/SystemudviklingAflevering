552b541354465f94f3f18f15c170d134a8d22da6
aebj <aebj27313@edu.ucl.dk>
2021-10-24 13:55:33 +0200

    phone & pc

--- pc.php
+++ pc.php
@@ -14,48 +14,48 @@ include('categories.php');
     <div class="pad">
       <div class="row">
         <div class="column">
-          <img src="img/acer1.png" alt="">
-          <h2>Acer Laptop</h2>
-          <p>Det her er en acer laptop</p>
+          <img src="img/pc1.jpg" alt="">
+          <h2>Asus ExpertCenter D700SA</h2>
+          <p>Pris: 6000 kr</p>
         </div>
         <div class="column">
-          <img src="img/acer1.png" alt="">
-          <h2>Acer Laptop</h2>
-          <p>Det her er en acer laptop</p>
+          <img src="img/pc2.jpg" alt="">
+          <h2>Acer Aspire XC-830</h2>
+          <p>Pris: 2000 kr</p>
         </div>
         <div class="column">
-          <img src="img/acer1.png" alt="">
-          <h2>Acer Laptop</h2>
-          <p>Det her er en acer laptop</p>
+          <img src="img/pc3.jpg" alt="">
+          <h2>Lenovo ThinkCentre M75q</h2>
+          <p>Pris: 5500 kr</p>
         </div>
         <div class="column">
-          <img src="img/acer1.png" alt="">
-          <h2>Acer Laptop</h2>
-          <p>Det her er en acer laptop</p>
+          <img src="img/pc4.jpg" alt="">
+          <h2>Asus ExpertCenter D900SA</h2>
+          <p>Pris: 13100 kr</p>
         </div>
       </div>
     </div>
     <div class="pad">
       <div class="row">
         <div class="column">
-          <img src="img/acer1.png" alt="">
-          <h2>Acer Laptop</h2>
-          <p>Det her er en acer laptop</p>
+          <img src="img/pc5.jpg" alt="">
+          <h2>Huawei MateStation</h2>
+          <p>Pris: 3600 kr</p>
         </div>
         <div class="column">
-          <img src="img/acer1.png" alt="">
-          <h2>Acer Laptop</h2>
-          <p>Det her er en acer laptop</p>
+          <img src="img/pc6.jpg" alt="">
+          <h2>Lenovo IdeaCentre 3</h2>
+          <p>Pris: 4400 kr</p>
         </div>
         <div class="column">
-          <img src="img/acer1.png" alt="">
-          <h2>Acer Laptop</h2>
-          <p>Det her er en acer laptop</p>
+          <img src="img/pc7.jpg" alt="">
+          <h2>HP OMEN 25L</h2>
+          <p>Pris: 11200 kr</p>
         </div>
         <div class="column">
-          <img src="img/acer1.png" alt="">
-          <h2>Acer Laptop</h2>
-          <p>Det her er en acer laptop</p>
+          <img src="img/pc8.jpg" alt="">
+          <h2>HP Pavilion TG01-2827</h2>
+          <p>Pris: 9500 kr</p>
         </div>
       </div>
     </div>
--- phone.php
+++ phone.php
@@ -14,48 +14,48 @@ include('categories.php');
     <div class="pad">
       <div class="row">
         <div class="column">
-          <img src="img/acer1.png" alt="">
-          <h2>Acer Laptop</h2>
-          <p>Det her er en acer laptop</p>
+          <img src="img/phone1.jpg" alt="">
+          <h2>Iphone 12</h2>
+          <p>Pris: 5000 kr</p>
         </div>
         <div class="column">
-          <img src="img/acer1.png" alt="">
-          <h2>Acer Laptop</h2>
-          <p>Det her er en acer laptop</p>
+          <img src="img/phone2.jpg" alt="">
+          <h2>SAMSUNG GALAXY M11</h2>
+          <p>Pris: 1100 kr</p>
         </div>
         <div class="column">
-          <img src="img/acer1.png" alt="">
-          <h2>Acer Laptop</h2>
-          <p>Det her er en acer laptop</p>
+          <img src="img/phone3.jpg" alt="">
+          <h2>HUAWEI P40 LITE</h2>
+          <p>Pris: 1700 kr</p>
         </div>
         <div class="column">
-          <img src="img/acer1.png" alt="">
-          <h2>Acer Laptop</h2>
-          <p>Det her er en acer laptop</p>
+          <img src="img/phone4.jpg" alt="">
+          <h2>SONY XPERIA 1 II</h2>
+          <p>Pris: 3700 kr</p>
         </div>
       </div>
     </div>
     <div class="pad">
       <div class="row">
         <div class="column">
-          <img src="img/acer1.png" alt="">
-          <h2>Acer Laptop</h2>
-          <p>Det her er en acer laptop</p>
+          <img src="img/phone5.jpg" alt="">
+          <h2>GOOGLE PIXEL 6</h2>
+          <p>Pris: 5100 kr</p>
         </div>
         <div class="column">
-          <img src="img/acer1.png" alt="">
-          <h2>Acer Laptop</h2>
-          <p>Det her er en acer laptop</p>
+          <img src="img/phone6.jpg" alt="">
+          <h2>NOKIA 2720 FLIP DUAL SIM</h2>
+          <p>Pris: 700 kr</p>
         </div>
         <div class="column">
-          <img src="img/acer1.png" alt="">
-          <h2>Acer Laptop</h2>
-          <p>Det her er en acer laptop</p>
+          <img src="img/phone7.jpg" alt="">
+          <h2>LG VELVET</h2>
+          <p>Pris: 2300 kr</p>
         </div>
         <div class="column">
-          <img src="img/acer1.png" alt="">
-          <h2>Acer Laptop</h2>
-          <p>Det her er en acer laptop</p>
+          <img src="img/phone8.jpg" alt="">
+          <h2>HTC DESIRE 21 PRO 5G</h2>
+          <p>Pris: 2600 kr</p>
         </div>
       </div>
     </div>
