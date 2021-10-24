f7a08e6e19349bfc18ecb0bcebd1abb636103d68
kasperUcl <73479558+KasperUcl@users.noreply.github.com>
2021-10-24 13:56:34 +0200

    Laptop Changes

--- index.php
+++ index.php
@@ -15,47 +15,47 @@ include('categories.php');
       <div class="row">
         <div class="column">
           <img src="img/acer1.png" alt="">
-          <h2>Acer Laptop</h2>
-          <p>Det her er en acer laptop</p>
+          <h2>Acer Aspire</h2>
+          <p>Pris: 4299kr</p>
         </div>
         <div class="column">
-          <img src="img/acer1.png" alt="">
-          <h2>Acer Laptop</h2>
-          <p>Det her er en acer laptop</p>
+          <img src="img/asus1.png" alt="">
+          <h2>Asus FX506LI</h2>
+          <p>Pris: 6799kr</p>
         </div>
         <div class="column">
-          <img src="img/acer1.png" alt="">
-          <h2>Acer Laptop</h2>
-          <p>Det her er en acer laptop</p>
+          <img src="img/lenovo1.png" alt="">
+          <h2>Lenovo Yoga Slim7</h2>
+          <p>Pris: 7992kr</p>
         </div>
         <div class="column">
-          <img src="img/acer1.png" alt="">
-          <h2>Acer Laptop</h2>
-          <p>Det her er en acer laptop</p>
+          <img src="img/msi1.png" alt="">
+          <h2>MSI Prestige 14</h2>
+          <p>Pris: 9799kr</p>
         </div>
       </div>
     </div>
     <div class="pad">
       <div class="row">
         <div class="column">
-          <img src="img/acer1.png" alt="">
-          <h2>Acer Laptop</h2>
-          <p>Det her er en acer laptop</p>
+          <img src="img/macbook1.png" alt="">
+          <h2>Macbook Pro 2020</h2>
+          <p>Pris: 10999kr</p>
         </div>
         <div class="column">
-          <img src="img/acer1.png" alt="">
-          <h2>Acer Laptop</h2>
-          <p>Det her er en acer laptop</p>
+          <img src="img/razer1.png" alt="">
+          <h2>Razer Blade 15</h2>
+          <p>Pris: 17999kr</p>
         </div>
         <div class="column">
-          <img src="img/acer1.png" alt="">
-          <h2>Acer Laptop</h2>
-          <p>Det her er en acer laptop</p>
+          <img src="img/huawei1.png" alt="">
+          <h2>Huawei Matebook 14</h2>
+          <p>Pris: 6999kr</p>
         </div>
         <div class="column">
-          <img src="img/acer1.png" alt="">
-          <h2>Acer Laptop</h2>
-          <p>Det her er en acer laptop</p>
+          <img src="img/microsoft1.png" alt="">
+          <h2>Microsoft Surface 14</h2>
+          <p>Pris: 5229kr</p>
         </div>
       </div>
     </div>
